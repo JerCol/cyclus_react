0be4a3bd11024c618345973bb546e5e67722aae7
JerCol <79970540+JerCol@users.noreply.github.com>
2025-05-30 10:29:11 +0000

    background added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import NewAttendeeForm from './components/NewAttendeeForm';
+import bg from './assets/background.png';   // Vite rewrites this to /assets/bg-ab12cd.png
+
 export default function App() {
   return (
-    <main className="p-8">
+    <main  className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${bg})` }}>
       <h1 className="text-2xl font-semibold mb-4">Add attendee</h1>
       <NewAttendeeForm />
     </main>
